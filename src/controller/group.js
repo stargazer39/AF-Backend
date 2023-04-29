f32d00565f39db28eacef286b1201a513dc77da6
KaushalyaPremarathne <it20633622@my.sliit.lk>
2023-04-29 07:08:11 +0100

    added api s

---
+++ src/controller/group.js
@@ -0,0 +1,109 @@
+let Group = require('../models/group')
+
+//create new group
+export function addNewGroup(req, res) {
+  const groupName = req.body.groupName
+  const description = req.body.description
+  const category = req.body.category
+  const groupIcon = req.body.groupIcon
+  const userId = req.body.userId
+
+  const newGroup = new Group({
+    groupName,
+    description,
+    category,
+    groupIcon,
+    userId,
+  })
+
+  newGroup
+    .save()
+    .then(() => {
+      res.json('Group Created')
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+}
+
+//fetch all groups
+export function getAllGroups(req, res) {
+  Group.find()
+    .then((groups) => {
+      res.json(groups)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+}
+
+//update group by id
+export function updateGroup(req, res, next) {
+  Group.findByIdAndUpdate(
+    req.params._id,
+    {
+      $set: req.body,
+    },
+    (error, data) => {
+      if (error) {
+        //return next(error)
+        console.log('group not found')
+      } else {
+        res.json(data)
+        console.log('group updated successfully !')
+      }
+    },
+  )
+}
+
+//delete the group
+export function deleteGroup(req, res) {
+  let _id = req.params._id
+
+  Group.findOneAndDelete(_id)
+    .then(() => {
+      res.json('deleted')
+    })
+    .catch((err) => {
+      console.log(err.message)
+    })
+}
+
+//filter by groupId
+export function getOneGroup(req, res) {
+  let _id = req.params._id
+
+  Group.findById(_id)
+    .then((group) => {
+      res.json(group)
+    })
+    .catch((err) => {
+      console.log(err.message)
+    })
+}
+
+//filter by category
+export function getGroupsByCategory(req, res) {
+  let category = req.params.category
+
+  Group.findOne({ category: category })
+    .then((group) => {
+      res.json(group)
+    })
+    .catch((err) => {
+      console.log(err.message)
+    })
+}
+
+//filter by userId
+export function getGroupsByUserId(req, res) {
+  let userId = req.params.userId
+
+  Group.findOne({ userId: userId })
+    .then((group) => {
+      res.json(group)
+    })
+    .catch((err) => {
+      console.log(err.message)
+    })
+}
---
+++ src/models/group.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose'
+
+const GroupSchema = new mongoose.Schema(
+  {
+    groupName: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: false,
+    },
+    category: {
+      type: String,
+      required: true,
+      lowercase: true,
+    },
+    groupIcon: {
+      type: [String],
+      //required: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' },
+  },
+)
+
+const Group = mongoose.model('Group', GroupSchema)
+
+module.exports = Group

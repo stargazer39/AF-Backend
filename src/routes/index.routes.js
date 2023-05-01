0a0fa8a318933e6898d6d48162db0401f5e650cf
Lakshika de Zoysa <dezoysalakshika813@gmail.com>
2023-05-01 16:56:39 +0530

    Add authRoute

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,9 +1,13 @@
 import express from 'express'
 import groupRouter from './group.routes'
 import questionRouter from './question.routes'
+import authRouter from './auth.routes'
+import userRouter from './user.routes'
 
 const router = express.Router()
 router.use('/group', groupRouter)
 router.use('/question', questionRouter)
+router.use('/auth', authRouter)
+router.use('/user', userRouter)
 
 export default router

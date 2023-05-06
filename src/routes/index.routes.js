25fe6caf108bcfa57ffbd3006999d6bb73e90472
Lakshika de Zoysa <dezoysalakshika813@gmail.com>
2023-05-06 16:30:10 +0530

    Add rating routes

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -4,6 +4,7 @@ import postRouter from './post.routes'
 import questionRouter from './question.routes'
 import authRouter from './auth.routes'
 import userRouter from './user.routes'
+import ratingRouter from './rating.routes'
 
 const router = express.Router()
 router.use('/group', groupRouter)
@@ -11,5 +12,6 @@ router.use('/post', postRouter)
 router.use('/question', questionRouter)
 router.use('/auth', authRouter)
 router.use('/user', userRouter)
+router.use('/rating', ratingRouter)
 
 export default router

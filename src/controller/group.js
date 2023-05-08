c63cf340d3b2b39137879cc4cfd56620a560a5e2
KaushalyaPremarathne <it20633622@my.sliit.lk>
2023-05-08 06:15:06 +0100

    added repository n service files

--- src/controller/group.js
+++ src/controller/group.js
@@ -1,7 +1,7 @@
 let Group = require('../models/group')
+const group = require('../services/group')
 
-//create new group
-export function addNewGroup(req, res) {
+function addNewGroup(req, res) {
   const groupName = req.body.groupName
   const description = req.body.description
   const category = req.body.category
@@ -9,28 +9,19 @@ export function addNewGroup(req, res) {
   const adminId = req.body.adminId
   const followersUserId = req.body.followersUserId
 
-  const newGroup = new Group({
-    groupName,
-    description,
-    category,
-    groupIcon,
-    adminId,
-    followersUserId,
-  })
-
-  newGroup
-    .save()
-    .then(() => {
-      res.json('Group Created')
+  group
+    .addNewGroup(groupName, description, category, groupIcon, adminId, followersUserId)
+    .then((result) => {
+      res.json(result)
     })
     .catch((err) => {
       console.log(err)
     })
 }
 
-//fetch all groups
-export function getAllGroups(req, res) {
-  Group.find()
+function getAllGroups(req, res) {
+  group
+    .getAllGroups()
     .then((groups) => {
       res.json(groups)
     })
@@ -39,91 +30,89 @@ export function getAllGroups(req, res) {
     })
 }
 
-//update group by id
-export function updateGroup(req, res, next) {
-  Group.findByIdAndUpdate(
-    req.params._id,
-    {
-      $set: req.body,
-    },
-    (error, data) => {
-      if (error) {
-        //return next(error)
-        console.log('group not found')
-      } else {
-        res.json(data)
-        console.log('group updated successfully !')
-      }
-    },
-  )
-}
+function updateGroup(req, res, next) {
+  const groupId = req.params._id
+  const updatedGroupData = req.body
 
-//delete the group
-export function deleteGroup(req, res) {
-  let _id = req.params._id
-
-  Group.findByIdAndDelete(_id)
-    .then(() => {
-      res.json('deleted')
+  group
+    .updateGroup(groupId, updatedGroupData)
+    .then((updatedGroup) => {
+      res.json(updatedGroup)
+      console.log('Group updated successfully!')
     })
     .catch((err) => {
-      console.log(err.message)
+      console.log(err)
+      res.status(500).send('Failed to update group')
     })
 }
 
-//filter by groupId
-export function getOneGroup(req, res) {
-  let _id = req.params._id
+function getOneGroup(req, res) {
+  const groupId = req.params._id
 
-  Group.findById(_id)
+  group
+    .getOneGroup(groupId)
+    .then((group) => {
+      res.json(group)
+    })
+    .catch((err) => {
+      console.log(err)
+      res.status(500).send('Failed to get group')
+    })
+}
+
+function deleteGroup(req, res) {
+  const groupId = req.params._id
+
+  group
+    .deleteGroup(groupId)
+    .then((message) => {
+      res.json(message)
+    })
+    .catch((err) => {
+      console.log(err)
+      res.status(500).send('Failed to delete group')
+    })
+}
+
+function addFollows(req, res) {
+  const groupId = req.params._id
+  const followers = req.body.followersUserId
+
+  group
+    .addFollows(groupId, followers)
+    .then((data) => {
+      res.json(data)
+    })
+    .catch((err) => {
+      console.log(err)
+      res.status(500).send('Failed to add followers to group')
+    })
+}
+
+// async function removeFollows(req, res) {
+//   try {
+//     const groupId = req.params._id
+//     const followerId = req.body.followerId
+//     const group = await groupService.removeFollows(groupId, followerId)
+//     res.json(group)
+//     console.log('group updated successfully !')
+//   } catch (err) {
+//     console.log(err.message)
+//   }
+// }
+
+function getGroupsByUserId(req, res) {
+  const adminId = req.params.adminId
+
+  group
+    .getGroupsByUserId(adminId)
     .then((group) => {
       res.json(group)
     })
     .catch((err) => {
       console.log(err.message)
+      res.status(500).send('Error retrieving group')
     })
-}
-
-//filter by category
-export function getGroupsByCategory(req, res) {
-  let category = req.params.category
-
-  Group.findOne({ category: category })
-    .then((group) => {
-      res.json(group)
-    })
-    .catch((err) => {
-      console.log(err.message)
-    })
-}
-
-//filter by userId
-export function getGroupsByUserId(req, res) {
-  let adminId = req.params.adminId
-
-  Group.findOne({ adminId: adminId })
-    .then((group) => {
-      res.json(group)
-    })
-    .catch((err) => {
-      console.log(err.message)
-    })
-}
-
-//patch followersUserId when follow button click
-export function addFollows(req, res) {
-  Group.findByIdAndUpdate(
-    req.params._id,
-    { $set: { followersUserId: req.body.followersUserId } },
-    (error, data) => {
-      if (error) {
-        console.log('group not found')
-      } else {
-        res.json(data)
-        console.log('group updated successfully !')
-      }
-    }
-  )
 }
 
 //remove followersUserId when click leave button
@@ -138,6 +127,17 @@ export function removeFollows(req, res) {
         res.json(data)
         console.log('group updated successfully !')
       }
-    }
+    },
   )
+}
+
+module.exports = {
+  addNewGroup,
+  getAllGroups,
+  updateGroup,
+  getOneGroup,
+  deleteGroup,
+  addFollows,
+  removeFollows,
+  getGroupsByUserId,
 }
--- src/routes/group.routes.js
+++ src/routes/group.routes.js
@@ -8,7 +8,7 @@ import {
   getOneGroup,
   getGroupsByUserId,
   addFollows,
-  removeFollows
+  removeFollows,
 } from '../controller/group'
 
 const groupRouter = express.Router()
@@ -17,9 +17,9 @@ groupRouter.post('/addgroup', addNewGroup)
 groupRouter.get('/getGroups', getAllGroups)
 groupRouter.patch('/updateGroup/:_id', updateGroup)
 groupRouter.delete('/deleteGroup/:_id', deleteGroup)
-groupRouter.get('/getGroupByCate/:category', getGroupsByCategory)
+// groupRouter.get('/getGroupByCate/:category', getGroupsByCategory)
 groupRouter.get('/getOneGroup/:_id', getOneGroup)
-groupRouter.get('/getGroupByUserId/:userId', getGroupsByUserId)
+//groupRouter.get('/getGroupByUserId/:userId', getGroupsByUserId)
 groupRouter.patch('/addFollows/:_id', addFollows)
 groupRouter.patch('/removeFollows/:_id', removeFollows)
 
